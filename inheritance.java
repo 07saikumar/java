cc3f1092b2042352328aeb87b0e7982b05515b5f
07saikumar <124706330+07saikumar@users.noreply.github.com>
2023-04-21 12:34:31 +0530

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -1,7 +1,7 @@
 class vehicles{
     int noOfwheels;
     String fuelType;
-    public void year(int year)
+    protected void year1()
     {
         System.out.println("these are vehilces");
     }
@@ -11,11 +11,13 @@ class bike extends vehicles{
     bike(){
     noOfwheels=2;
     fuelType="petorl";
-    
+    year1();
+    year1();
+    year1();
     int year=2023;
     }
     String engineType;
-    public void year(int year)
+    public void years(int year)
     {
         System.out.println("it has "+noOfwheels+" wheels and fuel type:"+fuelType);
         System.out.println("this is "+year+" model");
@@ -27,21 +29,53 @@ class sportsbike extends bike{
     noOfwheels=2;
     fuelType="petorl";
     int year=2024;
-    
-    engineType="4 storke engine";
+    year1();
+    year1();
+    year1();
+    String engineType="4 storke engine";
     }
     public void year(int year)
     {
         System.out.println("it has "+noOfwheels+" wheels and fuel type:"+fuelType);
         System.out.println("this is "+year+" model the engine type is: "+engineType);
     }
+    
 }
+class nakedbikes extends sportsbike{
+    nakedbikes()
+    {
+    year1();
+    year1();
+    year1();
+    }
+    
+    void display(){
+    System.out.println("this is naked bikes");
+    }
+}
+class vintagebieks extends nakedbikes
+{
+    vintagebieks()
+    {
+        year1();
+    year1();
+    year1();
+    }
+    void display1(){
+        System.out.println("this is vintage bikes");
+    }
+}
+
 
 public class main{
     public static void main(String[] args){
-        bike Bike = new bike();
-        Bike.year(2023);
-        sportsbike spb = new sportsbike();
+       
+        // sportsbike spb = new sportsbike();
+        vintagebieks spb = new vintagebieks();
+        spb.display();
         spb.year(2024);
+        spb.years(2024);
+        spb.year1();
+        spb.display1();
     }
 }
